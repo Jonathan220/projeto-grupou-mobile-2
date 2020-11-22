4217c9057416341fd92434b774bee87a13c78db8
Jonathan <jonathan@pop-os.localdomain>
2020-11-22 15:36:05 -0300

    chat salvando no firebase

--- pages/chat/index.js
+++ pages/chat/index.js
@@ -1,8 +1,32 @@
-import React, {useContext} from 'react';
-import { Container, ContainerNome, TextoLogo } from './style';
+import React, {useContext, useState} from 'react';
+import { Container, ContainerNome, TextoLogo, CaixaBotoes, BotaoCadastro, TextoBotaoCadastro, CampoTexto, ContainerMensagem, Mensagem, CaixaMensagemMaior, CaixaMensagem } from './style';
 import {UsuarioContext} from '../../contexts/user';
+import firebase from 'firebase';
+import 'firebase/firestore';
 
 const Chat = () => {
+    const [mensagem, setMensagem] = useState(["um", "doi", "olá galera qunato tempo, como vão eestá tudo beeeeem?"]);
+    const [mensagemEnviada, setMensagemEnviada] = useState("olá");
+
+    const HandleMensagem = () => {
+        if(mensagemEnviada == ""){
+            console.warn("Prencha o campo");
+            return
+        }
+
+        try {
+            firebase.firestore().collection('mensagens').add(
+                {
+                    texto: mensagemEnviada,
+                    lida: false,
+                    autor: user.email
+                }
+            )
+        } catch (error) {
+            console.warn("erro de comunicação, tente mais tarde");
+        }
+    }
+
     const {user} =useContext(UsuarioContext);
     return (
         <Container>
@@ -11,6 +35,25 @@ const Chat = () => {
                     {user.email}
                 </TextoLogo>
             </ContainerNome>
+            <ContainerMensagem>
+                {mensagem.map(msg => 
+                    <CaixaMensagemMaior>
+                        <CaixaMensagem>
+                            <Mensagem>
+                                {msg}
+                            </Mensagem>
+                        </CaixaMensagem>  
+                    </CaixaMensagemMaior>
+                )}
+            </ContainerMensagem>
+            <CaixaBotoes >
+                <CampoTexto placeholder="Digite sua mensagem..." value={mensagemEnviada} onChangeText={texto => setMensagemEnviada(texto)}/>
+                <BotaoCadastro invert={true} onPress={()=>{HandleMensagem()}}>
+                    {
+                        <TextoBotaoCadastro invert={true}>Enviar</TextoBotaoCadastro>
+                    }
+                </BotaoCadastro>
+            </CaixaBotoes>
         </Container>
     )
 }
--- pages/chat/style.js
+++ pages/chat/style.js
@@ -20,3 +20,62 @@ export const TextoLogo = styled.Text`
     text-align: center;
     font-size: 18px;
 `
+
+export const CaixaBotoes = styled.View`
+    flex-direction: row;
+    align-items: center;
+    width:100%;
+`
+export const BotaoCadastro = styled.TouchableOpacity`
+    background-color: ${props => props.invert ? "#AE1B73" : "#fff"};
+    border: 1px solid #AE1B73;
+    height: 50px;
+    border-radius: 5px;
+    justify-content: center;
+    align-items: center;
+    margin: 0px 5px;
+    flex: 1;
+`
+
+export const TextoBotaoCadastro = styled.Text`
+    color: ${props => props.invert ? "#fff" : "#ae1b73"};
+    font-size: 16px;
+    font-weight: bold;
+`
+
+export const CampoTexto = styled.TextInput`
+    height: 60px;
+    border-color: #ccc;
+    border-width: 1px;
+    padding: 8px 5px;
+    margin: 6px 5px;
+    border-radius: 5px;
+    flex: 3;
+    background-color: #fff;
+`
+export const ContainerMensagem = styled.View`
+    flex: 1;
+`
+
+export const Mensagem = styled.Text`
+    font-size: 14px;
+`
+
+
+export const CaixaMensagemMaior = styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: flex-end;
+    padding: 10px;
+`
+
+export const CaixaMensagem = styled.View`
+    background-color: #ccc;
+    width: 80%;
+    padding: 20px;
+    border-radius: 5px;
+    flex-direction: row;
+`
+export const AutorMensagem = styled.Text`
+
+`
